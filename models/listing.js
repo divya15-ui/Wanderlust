fb7c095757fc2920e3e0a6904ff819e7f5dc6bc1
Divya Sharma <divyasharma07771@gmail.com>
2025-10-08 20:23:54 +0530

    Add Project Files

---
+++ models/listing.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const reviews = require("./review.js");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  
+  category: {
+    type: String,
+    enum: ["Trending","Rooms","Iconic Cities", "Mountains", "Castles", "Amazing Pools","Camping","Farms","Arctic"],
+  },
+
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await reviews.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
+
